f5af50f33ba94ce15074664b8593eda4e33f062a
Rossi Oddet <rossi.oddet@gmail.com>
2017-04-12 09:13:53 +0200

    exemple de test

---
+++ src/test/java/java8/ex01/Exemple_Test.java
@@ -0,0 +1,18 @@
+package java8.ex01;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Exemple Test
+ */
+public class Exemple_Test {
+	
+	@Test
+	public void testQuiEchoue() {
+		fail();
+	}
+
+  
+}

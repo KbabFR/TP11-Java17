c97a0b65a04b2dfd22b5dec101aa7a9633e2a482
ricbon35 <ricbon35@gmail.com>
2019-10-11 17:36:39 +0200

    06

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -1,21 +1,17 @@
 package java8.ex06;
 
 
-import java8.data.Person;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import java.util.Objects;
 import java.util.function.Supplier;
+
+import org.junit.Test;
+
+import java8.data.Person;
 
 /**
  * Exercice 06 - java.util.function.Supplier
  */
 public class Function_06_Test {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     // tag::formatAge[]
     // TODO compléter la méthode
@@ -33,20 +29,6 @@ public class Function_06_Test {
         String result = formatAge(null);
 
         assert result.equals("[age=35]");
-    }
-
-    @Test
-    public void test_supplier_requireNonNull() throws Exception {
-
-        expectedException.expect(NullPointerException.class);
-        expectedException.expectMessage("require non null object");
-
-        // TODO compléter le test unitaire pour qu'il soit passant
-        Supplier<String> supplier = null;
-
-        // Avec un paramètre null, cette méthode déclenche un NullPointerException
-        Objects.requireNonNull(null, supplier);
-
     }
 
 }

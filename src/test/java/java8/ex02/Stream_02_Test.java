b32ca1c75baa651281449678d66109f3c6cf2884
RichardBONNAMY <rbonnamy@diginamic.fr>
2021-06-30 16:13:53 +0200

    Changement de nom d'une méthode pour être plus précis

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -1,31 +1,29 @@
 package java8.ex02;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+
 import java8.data.Data;
 import java8.data.domain.Customer;
 import java8.data.domain.Order;
-import java8.data.domain.Pizza;
-import org.junit.Test;
-
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
 
 /**
  * Exercice 02 - Transformation
  */
 public class Stream_02_Test {
 
-    @Test
-    public void test_map() throws Exception {
+	@Test
+	public void test_map_distinct() throws Exception {
 
-        List<Order> orders = new Data().getOrders();
+		List<Order> orders = new Data().getOrders();
 
-        // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+		// Trouver la liste des clients ayant déjà passés une commande
+		List<Customer> result = null;
 
-        assertThat(result, hasSize(2));
-    }
+		assertThat(result, hasSize(2));
+	}
 }
